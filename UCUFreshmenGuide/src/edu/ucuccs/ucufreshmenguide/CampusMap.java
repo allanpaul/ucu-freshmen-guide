6fbb2b5cd609506b0f23e7a02826d5b643bcf037
allanpaul <allanpaulguiting@yahoo.com.ph>
2014-10-01 00:46:31 +0800

    7th Commit
    little bit changes in the map.

--- UCUFreshmenGuide/src/edu/ucuccs/ucufreshmenguide/CampusMap.java
+++ UCUFreshmenGuide/src/edu/ucuccs/ucufreshmenguide/CampusMap.java
@@ -1,17 +1,22 @@
 package edu.ucuccs.ucufreshmenguide;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.ui.IconGenerator;
 
@@ -28,8 +33,7 @@ public class CampusMap extends SherlockFragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Get the view from tab_normal_map.xml
-		View view = inflater.inflate(R.layout.campus_map, container,
-				false);
+		View view = inflater.inflate(R.layout.campus_map, container, false);
 
 		// Gets the MapView from the XML layout and creates it
 		mapView = (MapView) view.findViewById(R.id.mapview);
@@ -48,78 +52,98 @@ public class CampusMap extends SherlockFragment {
 		// Set the map type to Satellite
 		map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 
-		IconGenerator iconFactory = new IconGenerator(getActivity());
+		IconGenerator icon = new IconGenerator(getActivity());
 
-		iconFactory.setStyle(IconGenerator.STYLE_GREEN);
-		addIcon(iconFactory, "Gymnasium", new LatLng(15.9786106, 120.5608737));
+		/*
+		 * iconFactory.setStyle(IconGenerator.STYLE_GREEN); addIcon(iconFactory,
+		 * "Gymnasium", new LatLng(15.9786106, 120.5608737));
+		 */
 
-		iconFactory.setStyle(IconGenerator.STYLE_ORANGE);
-		addIcon(iconFactory, "Orata Building", new LatLng(15.9803493,
-				120.5603938));
+		icon.setStyle(IconGenerator.STYLE_BLUE);
+		Bitmap iconBitmap = icon.makeIcon("Gymnasium");
 
-		iconFactory.setStyle(IconGenerator.STYLE_PURPLE);
-		addIcon(iconFactory, "New Building",
-				new LatLng(15.9799419, 120.5607604));
+		map.addMarker(new MarkerOptions()
+				.icon(BitmapDescriptorFactory.fromBitmap(iconBitmap))
+				.position(new LatLng(15.9786106, 120.5608737)).title("View"));
+		
+		map.setOnInfoWindowClickListener(new OnInfoWindowClickListener() {
+			public void onInfoWindowClick(Marker marker) {
+				
+				Toast toast = new Toast(getActivity());
+				ImageView view = new ImageView(getActivity());
+				view.setImageResource(R.drawable.ucu_gymn);
+				toast.setDuration(Toast.LENGTH_LONG);
+				toast.setView(view);
+				toast.show();
+				
+			}
+		});
 
-		iconFactory.setStyle(IconGenerator.STYLE_BLUE);
-		addIcon(iconFactory, "Engineering Building", new LatLng(15.9801533,
-				120.5602726));
-
-		iconFactory.setStyle(IconGenerator.STYLE_ORANGE);
-		addIcon(iconFactory, "Orata Building 2", new LatLng(15.979417,
-				120.560476));
-
-		iconFactory.setStyle(IconGenerator.STYLE_GREEN);
-		addIcon(iconFactory, "Quadrangle", new LatLng(15.9799959, 120.5610247));
-
-		iconFactory.setStyle(IconGenerator.STYLE_PURPLE);
-		addIcon(iconFactory, "AVR", new LatLng(15.9802868, 120.5617505));
-
-		iconFactory.setStyle(IconGenerator.STYLE_GREEN);
-		addIcon(iconFactory, "Accounting, Cashier and President Office",
-				new LatLng(15.9803681, 120.5615921));
-
-		iconFactory.setStyle(IconGenerator.STYLE_GREEN);
-		addIcon(iconFactory, "Green Home", new LatLng(15.9796589, 120.5607035));
-
-		iconFactory.setStyle(IconGenerator.STYLE_GREEN);
-		addIcon(iconFactory, "Green Home 2", new LatLng(15.979793, 120.5603709));
-
-		iconFactory.setStyle(IconGenerator.STYLE_ORANGE);
-		addIcon(iconFactory, "MAPEH Office",
-				new LatLng(15.9782706, 120.5608631));
-
-		iconFactory.setStyle(IconGenerator.STYLE_BLUE);
-		addIcon(iconFactory, "Comfort Rooom", new LatLng(15.9784087,
-				120.5609801));
-
-		iconFactory.setStyle(IconGenerator.STYLE_BLUE);
-		addIcon(iconFactory, "Comfort Rooom", new LatLng(15.9794789,
-				120.5607565));
-
-		iconFactory.setStyle(IconGenerator.STYLE_PURPLE);
-		addIcon(iconFactory, "Canteen", new LatLng(15.9797863, 120.561579));
-
-		iconFactory.setStyle(IconGenerator.STYLE_GREEN);
-		addIcon(iconFactory, "Orata Park", new LatLng(15.9799288, 120.5615552));
-
-		iconFactory.setStyle(IconGenerator.STYLE_BLUE);
-		addIcon(iconFactory, "Registrar Office", new LatLng(15.9800837,
-				120.5616952));
-
-		iconFactory.setStyle(IconGenerator.STYLE_BLUE);
-		addIcon(iconFactory, "DBC Room", new LatLng(15.9805233, 120.561192));
-
-		iconFactory.setStyle(IconGenerator.STYLE_GREEN);
-		addIcon(iconFactory, "Honasan Hall",
-				new LatLng(15.9804238, 120.5610754));
-
-		iconFactory.setStyle(IconGenerator.STYLE_PURPLE);
-		addIcon(iconFactory, "Badar", new LatLng(15.9805181, 120.56098));
-
-		iconFactory.setStyle(IconGenerator.STYLE_ORANGE);
-		addIcon(iconFactory, "Square Garden",
-				new LatLng(15.9804696, 120.560826));
+		/*
+		 * .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
+		 * iconFactory.setStyle(IconGenerator.STYLE_ORANGE);
+		 * addIcon(iconFactory, "Orata Building", new LatLng(15.9803493,
+		 * 120.5603938));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_PURPLE);
+		 * addIcon(iconFactory, "New Building", new LatLng(15.9799419,
+		 * 120.5607604));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_BLUE); addIcon(iconFactory,
+		 * "Engineering Building", new LatLng(15.9801533, 120.5602726));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_ORANGE);
+		 * addIcon(iconFactory, "Orata Building 2", new LatLng(15.979417,
+		 * 120.560476));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_GREEN); addIcon(iconFactory,
+		 * "Quadrangle", new LatLng(15.9799959, 120.5610247));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_PURPLE);
+		 * addIcon(iconFactory, "AVR", new LatLng(15.9802868, 120.5617505));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_GREEN); addIcon(iconFactory,
+		 * "Accounting, Cashier and President Office", new LatLng(15.9803681,
+		 * 120.5615921));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_GREEN); addIcon(iconFactory,
+		 * "Green Home", new LatLng(15.9796589, 120.5607035));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_GREEN); addIcon(iconFactory,
+		 * "Green Home 2", new LatLng(15.979793, 120.5603709));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_ORANGE);
+		 * addIcon(iconFactory, "MAPEH Office", new LatLng(15.9782706,
+		 * 120.5608631));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_BLUE); addIcon(iconFactory,
+		 * "Comfort Rooom", new LatLng(15.9784087, 120.5609801));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_BLUE); addIcon(iconFactory,
+		 * "Comfort Rooom", new LatLng(15.9794789, 120.5607565));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_PURPLE);
+		 * addIcon(iconFactory, "Canteen", new LatLng(15.9797863, 120.561579));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_GREEN); addIcon(iconFactory,
+		 * "Orata Park", new LatLng(15.9799288, 120.5615552));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_BLUE); addIcon(iconFactory,
+		 * "Registrar Office", new LatLng(15.9800837, 120.5616952));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_BLUE); addIcon(iconFactory,
+		 * "DBC Room", new LatLng(15.9805233, 120.561192));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_GREEN); addIcon(iconFactory,
+		 * "Honasan Hall", new LatLng(15.9804238, 120.5610754));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_PURPLE);
+		 * addIcon(iconFactory, "Badar", new LatLng(15.9805181, 120.56098));
+		 * 
+		 * iconFactory.setStyle(IconGenerator.STYLE_ORANGE);
+		 * addIcon(iconFactory, "Square Garden", new LatLng(15.9804696,
+		 * 120.560826));
+		 */
 
 		return view;
 	}
